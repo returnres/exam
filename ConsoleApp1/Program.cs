3e352aac8dc4d936101b854aae1da6a112e8e968
robertoi <>
2020-06-02 17:42:06 +0200

    threadpool

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,10 +11,30 @@ namespace ConsoleApp1
     class Program
     {
         private static object lockobj = new object();
+        private static object lockMon = new object();
         static int n = 0;
         static int n1 = 0;
+        static int n2 = 0;
+
         static void Main(string[] args)
         {
+
+
+            #region thread
+            for (int i = 0; i < 5; i++)
+            {
+                ThreadPool.QueueUserWorkItem(LongOperation);
+            }
+
+            //manca il mutex
+            Thread threada = new Thread(IncrMon);
+            Thread threadb = new Thread(IncrMon);
+            threada.Start();
+            threadb.Start();
+            threada.Join();
+            threadb.Join();
+            Console.WriteLine("n={0}", n2);
+
             Thread thread1 = new Thread(Incr);
             Thread thread2 = new Thread(Incr);
             thread1.Start();
@@ -26,7 +46,7 @@ namespace ConsoleApp1
             Thread t = new Thread(() =>
             {
                 Console.WriteLine(Thread.CurrentThread.Name);
-                    Thread.Sleep(1000);
+                Thread.Sleep(1000);
             });
 
             Thread t1 = new Thread(() =>
@@ -40,7 +60,7 @@ namespace ConsoleApp1
                         Thread.Sleep(1);
                         Console.WriteLine("n={0}", n);
                     }
-                   }
+                }
             });
             Thread t2 = new Thread(() =>
             {
@@ -62,8 +82,15 @@ namespace ConsoleApp1
             t1.Start();
             t2.Start();
             t.Join();//il codice si blocca e attende che t finisce
+
+            #endregion
             Console.ReadLine();
 
+        }
+        private static void LongOperation(object state)
+        {
+            Thread.Sleep(1000);
+            Console.WriteLine(state);
         }
 
         static void Incr()
@@ -72,6 +99,28 @@ namespace ConsoleApp1
             Interlocked.Increment(ref Program.n1);
             Thread.Sleep(1);
 
+        }
+
+        static void IncrMon()
+        {
+            bool locktake = false;
+            Monitor.TryEnter(lockMon, 1000, ref locktake);
+            if (locktake)
+            {
+                Thread.Sleep(2000);//se scommento non prendo ock perchè supera il secondo di attesa
+                try
+                {
+                    n2++;
+                }
+                finally
+                {
+                    Monitor.Exit(lockMon);
+                }
+            }
+            else
+            {
+                Console.WriteLine("lock non acquisito");
+            }
         }
     }
 }
