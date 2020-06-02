0e4df0d9bdde5ce1e5fb745503c156b385d3974f
robertoi <>
2020-06-02 18:49:35 +0200

    task

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading;
@@ -18,7 +19,112 @@ namespace ConsoleApp1
 
         static void Main(string[] args)
         {
+            #region task
 
+            //manca continuewhenall
+            //continuewheany
+            //task figli innestati
+            Task tx = Task.Run(() =>
+            {
+                Thread.Sleep(1000);
+            });
+            Task ty = Task.Run(() =>
+            {
+                Thread.Sleep(1000);
+            });
+
+
+            Task taskall = Task.WhenAll((new Task[] {tx,ty }));
+            //Task taskall = Task.WhenAny((new Task[] { t1, t2 }));
+            taskall.Wait();
+
+            Task<String> webtask = Task.Run(() =>
+            {
+                string url = "http://www.google.it";
+                WebClient wc = new WebClient();
+                return wc.DownloadString(url);
+            });
+
+            Task<String> headhtml =  webtask.ContinueWith<string>(x =>
+            {
+                var res = x.Result;
+                //fai qualcosa con res
+                return res;
+            });
+
+            var tokenSource = new CancellationTokenSource();
+            CancellationToken token = tokenSource.Token;
+
+            Task cancellabletask = null;
+            try
+            {
+                cancellabletask = Task.Run(() =>
+               {
+                   token.ThrowIfCancellationRequested();
+                   for (int i = 0; i < 2; i++)
+                   {
+                       Thread.Sleep(100);
+                       if (token.IsCancellationRequested)
+                       {
+                           //eseguo altre cose
+                           token.ThrowIfCancellationRequested();
+                       }
+                   }
+               }, token);
+
+                tokenSource.CancelAfter(1000);
+                cancellabletask.Wait();
+            }
+            catch (AggregateException aggregateException)
+            {
+                if (aggregateException.InnerException is OperationCanceledException)
+                {
+                    Console.WriteLine("OperationCanceledException£");
+
+                }
+                if (cancellabletask != null && (cancellabletask.IsCanceled && cancellabletask.Status == TaskStatus.Canceled))
+                {
+                    Console.WriteLine(aggregateException.ToString());
+                }
+            }
+
+
+            Task<String> task = Task.Run(() =>
+            {
+                string url = "http://www.google.it";
+                WebClient wc = new WebClient();
+                return wc.DownloadString(url);
+            });
+            var htmlUrl = task.Result;//blocco
+
+            List<Task> tasks = new List<Task>();
+            for (int i = 0; i < 5; i++)
+            {
+                int number = i;
+                tasks.Add(
+                   Task.Run(() =>
+                    {
+                        Console.WriteLine(number);
+                        Thread.Sleep(1000);
+                    }));
+            }
+
+            //Task.WaitAll(tasks.ToArray());
+            Task.WaitAny(tasks.ToArray());
+            Console.WriteLine("all task complete");
+
+            Task longtask = Task.Run(() =>
+             {
+                 Thread.Sleep(1000);
+             });
+            longtask.Wait(1000);
+
+
+            Task verylong = Task.Factory.StartNew(() =>
+            {
+                Thread.Sleep(2000);
+            }, TaskCreationOptions.LongRunning);
+            #endregion
 
             #region thread
             for (int i = 0; i < 5; i++)
